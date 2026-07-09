4521d7dc826449bac025578e8bd9f02db824a458
longvibol <vibollong@gmail.com>
2026-07-09 16:43:57 +0700

    df'

---
+++ softcode/services-demo/src/app/services/counter.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Counter {
+
+  private count: number = 0;
+
+  getCount():number {
+    return this.count;
+  }
+
+  increment(): void {
+    this.count++;
+  }
+
+  decrement(): void {
+    this.count--;
+  }
+
+  reset(): void {
+    this.count = 0;
+  }
+  
+}
